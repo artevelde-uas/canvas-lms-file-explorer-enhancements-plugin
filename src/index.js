64011a1f13828178ba95f48bb5b2a586a505eddb
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-05-13 14:28:11 +0200

    Use async/await

--- src/index.js
+++ src/index.js
@@ -6,30 +6,30 @@ export default function ({ router, dom }) {
         'profile.files',
         'groups.files',
         'courses.files'
-    ], () => {
-        Promise.all([
+    ], async () => {
+        let [buttons, selectAllCheckbox] = await Promise.all([
             dom.onElementReady('.ef-header .ef-header__secondary > .ui-buttonset'),
             dom.onElementReady('.ef-main #selectAllCheckbox')
-        ]).then(([buttons, selectAllCheckbox]) => {
-            buttons.insertAdjacentHTML('afterbegin',
-                `<button type="button" class="ui-button btn-select" title="Select all" data-tooltip="top" data-html-tooltip-title="Select all">
-                     <i class="icon-check-mark"></i>
-                 </button>`);
+        ]);
 
-            buttons.querySelector('.btn-select').addEventListener('click', () => {
-                selectAllCheckbox.click();
-            });
+        buttons.insertAdjacentHTML('afterbegin',
+            `<button type="button" class="ui-button btn-select" title="Select all" data-tooltip="top" data-html-tooltip-title="Select all">
+                 <i class="icon-check-mark"></i>
+             </button>`);
+
+        buttons.querySelector('.btn-select').addEventListener('click', () => {
+            selectAllCheckbox.click();
         });
 
-        dom.onElementReady('.ef-directory').then(directory => {
-            // Disable click handler on label element
-            directory.addEventListener('click', event => {
-                if (event.target.matches('label')) {
-                    event.stopPropagation();
-                }
-            }, {
-                useCapture: true
-            });
+        let directory = await dom.onElementReady('.ef-directory');
+
+        // Disable click handler on label element
+        directory.addEventListener('click', event => {
+            if (event.target.matches('label')) {
+                event.stopPropagation();
+            }
+        }, {
+            useCapture: true
         });
     });
 }
