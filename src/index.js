d0ccdc1753a890ab861064926b705bef22cf5b0d
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-05-13 14:23:03 +0200

    Use new services

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import './index.css';
 
 
-export default function (app) {
-    app.addRouteListener([
+export default function ({ router, dom }) {
+    router.onRoute([
         'profile.files',
         'groups.files',
         'courses.files'
     ], () => {
         Promise.all([
-            app.addReadyListener('.ef-header .ef-header__secondary > .ui-buttonset'),
-            app.addReadyListener('.ef-main #selectAllCheckbox')
+            dom.onElementReady('.ef-header .ef-header__secondary > .ui-buttonset'),
+            dom.onElementReady('.ef-main #selectAllCheckbox')
         ]).then(([buttons, selectAllCheckbox]) => {
             buttons.insertAdjacentHTML('afterbegin',
                 `<button type="button" class="ui-button btn-select" title="Select all" data-tooltip="top" data-html-tooltip-title="Select all">
                      <i class="icon-check-mark"></i>
                  </button>`);
-            
+
             buttons.querySelector('.btn-select').addEventListener('click', () => {
                 selectAllCheckbox.click();
             });
         });
-        
-        app.addReadyListener('.ef-directory').then(directory => {
-            // Disable click handler on label element 
+
+        dom.onElementReady('.ef-directory').then(directory => {
+            // Disable click handler on label element
             directory.addEventListener('click', event => {
                 if (event.target.matches('label')) {
                     event.stopPropagation();
